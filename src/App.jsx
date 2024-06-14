3afbafdfad4b66c35fa926b451817969c22fc5f5
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-14 19:44:37 -0300

    Componente App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@fortawesome/fontawesome-svg-core';
+import './App.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Teste</h1>
+    </div>
+  );
+}
+
+export default App;
