5e275ba78f7d1b99435898dff81982874f03fd2f
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-15 21:09:33 -0300

    Componente Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import '@fortawesome/fontawesome-svg-core';
 import './App.css';
 
 import React from 'react';
 import Todo from './todo/todo';
 import About from './about/about';
+import Menu from './template/menu';
 
 function App() {
   return (
     <div className='container'>
+      <Menu/>
       <Todo />
       <About/>
     </div>
