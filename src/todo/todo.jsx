56ea4ef15574cf8f9f176f3901fd4b2a0cfe8f81
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-17 20:56:41 -0300

    Componente TodoForm

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './TodoForm'
 
 export default class Todo extends Component {
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
+                <TodoForm/>
             </div>
         )
     }
