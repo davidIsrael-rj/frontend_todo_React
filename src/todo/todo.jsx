cad67cbee4d6170d8e41a56c1c0439eaca51915b
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-18 20:52:03 -0300

    Evento Adicionar

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,12 +4,22 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 
 export default class Todo extends Component {
-    render(){
-        return(
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    
+    handleAdd() {
+        console.log('Add')
+    }
+
+    render() {
+        return (
             <div>
-                <PageHeader name='Tarefas' small='Cadastro'/>
-                <TodoForm/>
-                <TodoList/>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm handleAdd={this.handleAdd} />
+                <TodoList />
             </div>
         )
     }
