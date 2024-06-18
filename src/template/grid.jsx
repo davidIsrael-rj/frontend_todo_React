d7e9268d38d607b30c73b6a1c8616a092b662ab9
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-18 18:03:51 -0300

    Components IconButton

--- src/template/grid.jsx
+++ src/template/grid.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default class Grid extends Componentes {
-    toCssClasses(number) {
-        const cols = number ? SVGAnimatedNumberList.split(' ') : []
+export default class Grid extends Component {
+    toCssClasses(numbers) {
+        const cols = numbers ? numbers.split(' ') : []
         let classes = ''
 
         if (cols[0]) classes += `col-xs-${cols[0]}`
