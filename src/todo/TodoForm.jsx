5a1e8af492c79928564467d2491756b58910548c
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-21 06:13:24 -0300

    Adicionar Teclas de Atalhos

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -2,27 +2,39 @@ import React from "react";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
-const TodoForm = props => (
-   <div role='form' className='todoForm'>
-      <Grid cols='12 9 10'>
-         <input id="description" className='form-control'
-            placeholder='Adicione uma Tarefa'
-            onChange={props.handleChange}
-            value={props.description}></input>
-      </Grid>
-      <Grid cols='12 3 2'>
-         <IconButton btnStyle='primary' icon='plus'
-            onClick={props.handleAdd} />
-         <IconButton
-            btnStyle='info'
-            icon='search'
-            onClick={props.handleSearch} />
-         <IconButton
-            btnStyle='default'
-            icon='close'
-            onClick={props.handleClear} />
-      </Grid>
-   </div>
-)
+const TodoForm = props => {
+   //Utilizando Teclas de Atalho
+   const keyHandler = (e) => {
+      if(e.key === 'Enter'){
+         e.shiftKey ? props.handleSearch() : props.handleAdd()
+      }else if (e.key === 'Escape'){
+         props.handleClear()
+      }
+   }
+
+   return (
+      <div role='form' className='todoForm'>
+         <Grid cols='12 9 10'>
+            <input id="description" className='form-control'
+               placeholder='Adicione uma Tarefa'
+               onKeyUp={keyHandler}
+               onChange={props.handleChange}
+               value={props.description}></input>
+         </Grid>
+         <Grid cols='12 3 2'>
+            <IconButton btnStyle='primary' icon='plus'
+               onClick={props.handleAdd} />
+            <IconButton
+               btnStyle='info'
+               icon='search'
+               onClick={props.handleSearch} />
+            <IconButton
+               btnStyle='default'
+               icon='close'
+               onClick={props.handleClear} />
+         </Grid>
+      </div>
+   )
+}
 
 export default TodoForm
