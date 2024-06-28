ae987790f096fa03d7b88587d5568bc612e01c22
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-28 07:23:02 -0300

    Texto para a pagina sobre

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -3,9 +3,16 @@ import PageHeader from '../template/pageHeader'
 
 const About = props => (
     <div>
-        <PageHeader name='Sobre' small='Nós'/>
-        <h2>Aqui contamos a nossa história </h2>
-        <p>Como surgiu a nossa Empresa</p>
+        <PageHeader name='Sobre' small='TodoApp' />
+        <h2>TodoApp - Gerenciamento de tarefas</h2>
+        <p>TodoApp é uma aplicação de gerenciamento de tarefas
+            que permite listar e organizar suas atividades diárias.
+            Com ela, você pode adicionar novas tarefas,
+            marcar como concluídas, desmarcar se necessário e
+            excluir tarefas quando não forem mais relevantes.
+            TodoApp facilita o controle e acompanhamento das suas
+            responsabilidades diárias, ajudando a aumentar sua produtividade.
+        </p>
     </div>
 )
 
