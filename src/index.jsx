4c8d130532473d820302510797823f8c90f91dd1
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-28 07:41:10 -0300

    Aplicando Middleware (Redux-Promise)

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 
 import App from './main/App'
 import rootReducer from './main/reducers'
 import { Provider } from 'react-redux'
+import promise from 'redux-promise'
 
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(rootReducer, devTools)
+const store = applyMiddleware(promise)(createStore)(rootReducer, devTools)
 
 
 ReactDOM.render(
